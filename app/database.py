d4fa8a408792a11d9ca07aeccf53d113465d8679
Ardhendu Pramanik <ardhendujj002@gmail.com>
2026-06-28 00:26:52 +0530

    feat: save resume text to Supabase via POST /resume

--- app/database.py
+++ app/database.py
@@ -1,3 +1,5 @@
+from typing import Generator
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
@@ -12,3 +14,12 @@ SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 class Base(DeclarativeBase):
     pass
+
+
+def get_db() -> Generator:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
